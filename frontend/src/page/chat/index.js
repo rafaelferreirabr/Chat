5ff0a7abf93b77bcdde607db659772ab2f1a6d2e
Romulo Reis <reisinfante@gmail.com>
2020-02-12 14:28:29 -0300

    no redux/rev - map render/socket io ok

--- frontend/src/page/chat/index.js
+++ frontend/src/page/chat/index.js
@@ -1,32 +1,78 @@
 import React, { Component } from 'react';
 import { ChatStyle } from './style';
+import socketIOClient from 'socket.io-client';
+// import axios from 'axios';
 
 class Chat extends Component {
     constructor(props) {
         super(props);
         this.state = {
             userName: [],
-            chat: '',
-            mainChat: []
+            mensagem: '',
+            dbMsg: [],
+            socket: this.io()
+
         };
     };
+
+    io(){
+        // const { endPoint } = this.state;
+        const socket = socketIOClient('http://10.0.10.254:3001');
+        return socket
+    }
 
     componentDidMount() {
         const userName = this.props.location.param;
         this.setState({ userName: userName });
-        // console.log(userName);
+
+        // console.log(this.props.location.param);
+
+        // const { endPoint } = this.state;
+        // const socket = socketIOClient(endPoint);
+        // const socket = this.io();
+        this.state.socket.on("mensagens anteriores", data => {this.setState({dbMsg:data})});
+
+        this.state.socket.on("mensagem criada", data => {this.setState({dbMsg:[...this.state.dbMsg, data]})});
+        // console.log(this.state.dbMsg);
+
+        // socket.emit('cria mensagem', 'oi');
+        // console.log(this.state.dbMsg);
+
+        // axios.get(this.state.endPoint2)
+        //     .then(res => {
+        //         this.setState({ dbMsg: res.data });
+        //         console.log(res.data);
+        //         console.log(this.state.dbMsg);
+        //     });
     }
 
     handleText = e => {
-        this.setState({ chat: e.target.value });
-        // console.log(this.state.chat);
+        this.setState({ mensagem: e.target.name });
+        // console.log(this.state.mensagem);
     }
 
     handleTextSubmit = e => {
         e.preventDefault();
-        this.setState({ mainChat: [...this.state.mainChat, this.state.chat] });
-        this.setState({ chat: '' });
-        // this.scroll();
+        // this.setState({mensagem: this._name.value});
+        // this.setState({ dbMsg: [...this.state.dbMsg, this.state.mensagem] });
+        const all = {nome:this.state.userName.userName, mensagem:this.state.mensagem}
+        // const socket = this.io();
+        this.state.socket.emit('cria mensagem', all);
+        this.setState({dbMsg:[...this.state.dbMsg, {nome: this.state.userName.userName, mensagem:this.state.mensagem}]});
+        this.setState({mensagem: ''});
+
+        // axios.post(this.state.endPoint2, { nome: this.state.userName.userName, mensagem: this.state.mensagem })
+        // console.log(this.state.userName.userName);
+        // console.log(this.state.mensagem);
+        // console.log(this.state.dbMsg);
+        // this.setState({ mainChat: [...this.state.mainChat, this.state.chat] });
+        // this.setState({ chat: '' });
+
+
+
+
+
+        // this.scroll();this.state.mensagem
         // console.log(this.state.userName);
 
     }
@@ -44,19 +90,29 @@ class Chat extends Component {
     //     console.log(res);
     // }
 
+
     render() {
         return (
             <ChatStyle>
                 <h1>Chat D5</h1>
-                <div id="chat-window" className="textContent">{this.state.mainChat.map(textChat => <ul>{this.state.userName.userName}:<li>{textChat}</li></ul>)}</div>
+                <div id="chat-window" className="textContent">
+                {console.log('kakakakak')}
+                    <ul>
+                        {this.state.dbMsg.reverse().map(textChat => <li key={textChat.id}><strong>{textChat.nome}:</strong> {textChat.mensagem} </li>)}
+                    </ul>
+                </div>
                 <div className="inputPosition"><input type="text" name="textChat"
                     autoComplete="off"
-                    onChange={this.handleText} value={this.state.chat} />
+                    onChange={this.handleText} 
+                    // ref={input => this._name = input}
+                    // value={this.state.mensagem}
+                    />
                     <button type="button"
                         // onKeyDown={this.handleTextSubmitKey}
                         onClick={this.handleTextSubmit}>Enviar</button>
                 </div>
             </ChatStyle>
+
         );
     }
 
