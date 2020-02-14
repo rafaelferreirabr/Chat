d308fc9e2b41c3a6d4c9a6dfad99df95190c2563
Rafael Ferreira <rafamarques03@gmail.com>
2020-02-14 17:12:19 -0300

    trocado os endpoints por sockets

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,48 +1,47 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 const { Mensagem } = require('./models')
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+const bodyParser = require('body-parser')
 
-// app.get('/', async (req, res) => {
-//    const mensagem = await Mensagem.create({nome:"manoel", mensagem:"teste 3"})
-//    res.json(mensagem)
-// })
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+io.origins(['*'])
+io.origins((origin, callback) => {
+    if (origin === 'https://foo.example.com') {
+      return callback('origin not allowed', false);
+    }
+    callback(null, true);
+  });
 
-app.get('/mensagem', async (req, res) => {
-    const mensagens = await Mensagem.findAll()
-    res.json(mensagens)
-}); 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS")
+    next()
+  })
 
-app.post('/mensagem', async (req, res) => {
-    const {nome, mensagem} = req.body
-    const msg = await Mensagem.create({nome, mensagem})
-    res.json(msg)
-}); // Criar
 
-app.get('/mensagem/:id', async (req, res) => {
-    const mensagem = Mensagem.findByPk(req.params.id)
-    res.json(mensagem)
-}); //Buscar
 
-app.put('/mensagem/:id', async (req, res) => {
-    const id = req.params.id
-    const {nome, mensagem} = req.body
-    const msg = await Mensagem.update({nome, mensagem}, {
-        where: {
-            id
-        }
+io.on('connection', (socket) =>{
+    Mensagem.findAll({limit: 10,order: [['id', 'DESC']]}).then((mensagens) => {
+        socket.emit('mensagens anteriores', mensagens)
     })
-    res.json(msg)
-}); //Editar
-app.delete('/mensagem/:id', (req, res) => {
-    const id = req.params.id
-    Mensagem.destroy({
-        where: {
-            id: id
-        }
+    console.log('a user is connected', socket.id)
+    socket.on('cria mensagem', (data) => {
+        const {nome, mensagem} = data
+        Mensagem.create({nome, mensagem})
+            .then(msg => {
+                socket.broadcast.emit('mensagem criada', msg.dataValues)
+            })
+            .catch(error => {
+                console.log({error: error})
+            })
     })
-});
+  })
 
-app.listen(port, () => {
+http.listen(port, () => {
     console.log(`App running on Port ${port}`)
 })
